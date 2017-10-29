71251ed1723fd505390a0bc7e4c2c00fabd68c89
Moustafa Naquib <naquibm@LabImage2016-17>
2017-10-29 15:06:36 -0700

    Initial commit

---
+++ app/src/main/java/edu/wwu/devimandir/devimandir/MainActivity.java
@@ -0,0 +1,54 @@
+package edu.wwu.devimandir.devimandir;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] sideMenuPages; // Array of strings containing names of side menu pages
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private Button hamburger;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get array of side menu pages from array.xml
+        sideMenuPages = getResources().getStringArray(R.array.side_menu_array);
+
+        // Get view by id for needed elements
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        hamburger = (Button) findViewById(R.id.hamburger);
+
+
+
+        // Create array adapter to insert array of page names into side menu
+        mAdapter = new ArrayAdapter<String>(this, R.layout.list_item_style, sideMenuPages);
+
+        mDrawerList.setAdapter(mAdapter);
+
+
+        hamburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDrawer();
+            }
+        });
+
+    }
+
+    public void openDrawer(){
+        mDrawerLayout.openDrawer(Gravity.LEFT);
+    }
+}
